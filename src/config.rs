d4ea427cfedbce9add0be24c71c83896c0ebd8a7
Tahir Chaudhry <ubuntuhasnoroblox@gmail.com>
2025-05-12 12:35:12 -0400

    Split out config into "general" and "filter" categories

--- src/config.rs
+++ src/config.rs
@@ -1,16 +1,35 @@
+use std::path::PathBuf;
+
 use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
 pub struct Config {
-    pub filter_is_whitelist: bool,
-    pub filter: FilterList,
+    pub general: GeneralConfig,
+    pub filter: FilterConfig,
 }
 
 #[derive(Deserialize, Debug)]
-pub struct FilterList {
+pub struct GeneralConfig {
+    pub game_path: String,
+    pub staging_path: String,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct FilterConfig {
+    pub filter_is_whitelist: bool,
     pub list: Vec<String>,
 }
 
 pub fn parse_config(raw: &str) -> Result<Config, toml::de::Error> {
     toml::from_str(raw)
+}
+
+/// Helper function to load a config from the current working directory.
+pub fn load_config_from_wd() -> Option<Config> {
+    let cfg_path: PathBuf = PathBuf::from("./seedbox.toml");
+    if let Ok(cfg_raw) = std::fs::read_to_string(&cfg_path) {
+        parse_config(&cfg_raw).ok()
+    } else {
+        None
+    }
 }
